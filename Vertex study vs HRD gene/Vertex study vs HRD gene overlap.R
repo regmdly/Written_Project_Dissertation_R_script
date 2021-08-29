47857d304bb365e91073ca898fe5e920f5dc343e
regmdly <89692950+regmdly@users.noreply.github.com>
2021-08-29 08:27:50 +0800

    Update Vertex study vs HRD gene overlap.R

--- Vertex study vs HRD gene/Vertex study vs HRD gene overlap.R
+++ Vertex study vs HRD gene/Vertex study vs HRD gene overlap.R
@@ -1,3 +1,8 @@
+## determine target HR related gene
+## Latest update: 25/5/2021
+## Version 1.0.0
+## Author: Dian Lyu
+
 Vortex_study = read.table("Vortex_study_V1_Exons_10bp_Targets.txt")
 HRD_gene = read.csv("HRD_gene_list.csv")
 HRD_gene[1,] %in% Vortex_study[,]
